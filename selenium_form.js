eb3615326764f794b14e8269fded6027574e093b
OlenaKamenetska <olena.kamen@gmail.com>
2020-10-27 17:56:05 +0200

    HW35_SF

--- selenium_form.js
+++ selenium_form.js
@@ -1,6 +1,25 @@
-// Open https://the-internet.herokuapp.com/login
+it('selenium_form', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
-// Please automate next test cases:
-// 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
-// 2. Login with invalid creds and check validation error
-// 3. Logout from app and assert you successfully logged out
+    cy.get('[type="text"]').type('tomsmith');
+    cy.get('[type="password"]').type('SuperSecretPassword!');   
+ 
+    cy.get ('.fa').click();
+    cy.get('.flash.success').contains('You logged into a secure area').should('exist')       
+})
+
+it('selenium_form', () => {
+cy.get('.icon-2x.icon-signout').click();
+cy.get('.flash.success').contains('You logged out of the secure area!').should('exist')    
+
+cy.get('[type="text"]').type('Olena');
+cy.get('[type="password"]').type('Kamen');    
+})
+
+
+it('selenium_form', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    
+    cy.get ('.fa').click();
+    cy.get('.flash.error').contains('Your username is invalid').should('exist');
+    })
